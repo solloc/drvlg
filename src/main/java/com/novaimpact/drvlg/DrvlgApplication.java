07a1f1dad6b3ed15672da0851a795f16507531d8
solloc <matthias@maisch.me>
2019-11-22 15:41:32 +0100

    initial commit of spring boot application

---
+++ src/main/java/com/novaimpact/drvlg/DrvlgApplication.java
@@ -0,0 +1,13 @@
+package com.novaimpact.drvlg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DrvlgApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DrvlgApplication.class, args);
+	}
+
+}
