de208010c76d2d61034d6d0016da522fc95bda99
solloc <matthias@maisch.me>
2019-12-05 20:03:06 +0100

    unsecured application

---
+++ src/main/java/com/novaimpact/drvlg/MvcConfig.java
@@ -0,0 +1,21 @@
+package com.novaimpact.drvlg;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * MvcConfig
+ */
+@Configuration
+public class MvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/home").setViewName("home");
+        registry.addViewController("/hello").setViewName("hello");
+        registry.addViewController("/login").setViewName("login");
+    }
+
+    
+}
